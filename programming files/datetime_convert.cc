b45f1b555ddff6fe8431764acbba1499a389c737
DocDomergue <97396135+DocDomergue@users.noreply.github.com>
2023-03-21 10:23:57 -0400

    partial comments

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -1,14 +1,24 @@
+// Include libraries
 #include <iostream>
 #include <iomanip>
 #include <sstream>
 #include <chrono>
 
+
 int main()
 {
+    // Initialize sample string
     std::string date_str = "2022-03-17 10:45:30";
+    
+    // Initialize date_obj
     std::tm date_obj = {};
+    
+    // Feed sample string to a string stream
     std::istringstream ss(date_str);
+    
+    // Parse in time from string string
     ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
+    
     std::stringstream formatted_date_ss;
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
     std::string formatted_date = formatted_date_ss.str();
