249a94aa031e909a7772d2a6e137b8b908c2a319
Gian <47407561+aiqojo@users.noreply.github.com>
2023-03-21 10:17:10 -0400

    Adding comments to make more readable

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,8 +1,10 @@
 a, b = 0, 1
 sum_even = 0
 while b < 4000000:
+    # If b is even, we add to sum
     if b % 2 == 0:
         sum_even += b
+    # Continuing fibonacci sequence
     a, b = b, a+b
 
 print(sum_even)
