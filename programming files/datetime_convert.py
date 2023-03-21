e76ba6a5960f118a514b65e3679c93fe911ba320
DocDomergue <97396135+DocDomergue@users.noreply.github.com>
2023-03-21 10:19:53 -0400

    Added comments

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,7 +1,14 @@
+# Pulling in datetime library
 from datetime import datetime
 
+# Sample time as a string
 date_str = "2022-03-17 10:45:30"
+
+# Read in string to create a datetime object
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+# Reformat date style
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
 
+# Print newly formatted date toString
 print(formatted_date)
