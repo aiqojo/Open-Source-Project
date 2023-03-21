bf1bc678d1712ec8126db1fb607faaf77687ed18
Gian <47407561+aiqojo@users.noreply.github.com>
2023-03-21 10:29:43 -0400

    Making bugs

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,10 +1,11 @@
+# Bugs Gian
 def rgb_to_hex(r, g, b):
     # Making sure values are within bounds
-    r = max(0, min(255, r))
+    r = min(0, max(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
     # Converts the ints to hex
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    return '{:02X}{:02X}{:02X}'.format(g, r, b)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
